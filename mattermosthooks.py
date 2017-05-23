6ce732052fd9922652cd39b4729901f335cc8c81
root <root@serv69.serv69>
2017-05-23 13:26:02 +0300

    adapted multiple hg branches

--- mattermosthooks.py
+++ mattermosthooks.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib2
 import json
+import os
 
 from collections import namedtuple
 from mercurial.cmdutil import show_changeset
@@ -11,6 +12,7 @@ Config = namedtuple(
     'HgMattermostHooksConfig',
     field_names=[
         'webhook_url',
+        'webhook_url_mainproject',
         'repo_name',
         'commit_url',
         'username',
@@ -20,19 +22,18 @@ Config = namedtuple(
 
 
 def get_config(ui):
-    webhook_url = ui.config(config_group, 'webhook_url')
-    repo_name = ui.config(config_group, 'repo_name', default=None)
-    commit_url = ui.config(config_group, 'commit_url', default=None)
-    username = ui.config(config_group, 'username', default="mercurial")
-    icon_emoji = ui.config(config_group, 'icon_emoji', default=None)
-    icon_url = ui.config(config_group, 'icon_url', default=None)
-
-    return Config(webhook_url, repo_name, commit_url, username, icon_emoji,
-                  icon_url)
+    return Config(
+        webhook_url = ui.config(config_group, 'webhook_url'),
+        webhook_url_mainproject = ui.config(config_group, 'webhook_url_mainproject'),
+        repo_name = ui.config(config_group, 'repo_name', default=None),
+        commit_url = ui.config(config_group, 'commit_url', default=None),
+        username = ui.config(config_group, 'username', default="mercurial"),
+        icon_emoji = ui.config(config_group, 'icon_emoji', default=None),
+        icon_url = ui.config(config_group, 'icon_url', default=None)
+    )
 
 
 def pushhook(node, hooktype, url, repo, source, ui, **kwargs):
-    username = ui.username()
     config = get_config(ui)
 
     changesets = get_changesets(repo, node)
@@ -40,10 +41,10 @@ def pushhook(node, hooktype, url, repo, source, ui, **kwargs):
     messages = render_changesets(ui, repo, changesets, config)
 
     ensure_plural = "s" if count > 1 else ""
-    ensure_repo_name = " to \"{0}\"".format(config.repo_name) if config.repo_name else ""
+    ensure_repo_name = " to \"{0}\"".format(os.path.basename(repo.root))
 
     text = "{user} pushes {count} changeset{ensure_plural}{ensure_repo_name}:\n{changes}".format(
-        user=username,
+        user=ui.username() or config.username,
         count=count,
         ensure_plural=ensure_plural,
         ensure_repo_name=ensure_repo_name,
@@ -55,11 +56,15 @@ def pushhook(node, hooktype, url, repo, source, ui, **kwargs):
 def get_changesets(repo, node):
     node_rev = repo[node].rev()
     tip_rev = repo['tip'].rev()
-    return range(tip_rev, node_rev - 1, -1)
+    return [
+        rev
+        for rev in range(tip_rev, node_rev - 1, -1)
+        if 'myproject' in repo[rev].branch()
+    ]
 
 
 def render_changesets(ui, repo, changesets, config):
-    url = config.commit_url
+    url = "http://192.168.200.69:8008/{}/rev/".format(os.path.basename(repo.root))
     if url:
         node_template = "<{url}{{node|short}}|{{node|short}}>".format(url=url)
     else:
@@ -68,8 +73,7 @@ def render_changesets(ui, repo, changesets, config):
     template = "{0}: ```{1}```\\n".format(node_template, " | ".join([
         "{branch}",
         "{date(date, '%Y-%m-%d [%H:%M:%S]')}",
-        "{desc|strip|firstline}",
-        # "{desc}",
+        "{desc|strip|firstline}"
     ]))
 
     displayer = show_changeset(ui, repo, {'template': template})
@@ -86,7 +90,8 @@ def post_message_to_mattermost(message, config):
     }
     payload_optional_key(payload, config, 'icon_url')
     payload_optional_key(payload, config, 'icon_emoji')
-    request = urllib2.Request(config.webhook_url, "payload={0}".format(json.dumps(payload)))
+    webhook_url = config.webhook_url
+    request = urllib2.Request(webhook_url, "payload={0}".format(json.dumps(payload)))
     urllib2.build_opener().open(request)
 
 

0fb2aef1ce3c3634182ff8527e61fcf9e306d67e
Maxim Nikolaev <simno87@gmail.com>
2017-05-26 15:01:21 +0300

    complete hook with multiple repositories and shared branches support

--- mattermosthooks.py
+++ mattermosthooks.py
@@ -14,12 +14,11 @@ Config = namedtuple(
     field_names=[
         'webhook_url',
         'branches',
-        'repo_name',
+        'commit_url',
         'username',
         'icon_emoji',
         'icon_url',
     ])
-
 
 
 def get_teams(ui):
@@ -42,11 +41,11 @@ def get_config(ui, team):
         branches = ui.configlist(
             config_group, get_field('branches')),
 
-        repo_name = ui.config(
-            config_group, 'repo_name', default=None),
+        commit_url = ui.config(
+            config_group, 'commit_url', default=None),
 
         username = ui.config(
-            config_group, 'username', default="mercurial"),
+            config_group, 'username', default=ui.username()),
 
         icon_emoji = ui.config(
             config_group, 'icon_emoji', default=None),
@@ -57,60 +56,65 @@ def get_config(ui, team):
 
 
 def pushhook(node, hooktype, url, repo, source, ui, **kwargs):
-    config = get_config(ui)
-    for team, changesets in get_changesets(repo, node).iteritems():
-        count = len(changesets)
-        messages = re.sub(
-            '[;]', '',
-            render_changesets(ui, repo, changesets, config)
-        )
-
-        ensure_plural = "s" if count > 1 else ""
-        ensure_repo_name = " to \"{0}\"".format(os.path.basename(repo.root))
-
-        text = "{user} pushes {count} changeset{ensure_plural}{ensure_repo_name}:\n{changes}".format(
-            user=ui.username() or config.username,
-            count=count,
-            ensure_plural=ensure_plural,
-            ensure_repo_name=ensure_repo_name,
-            changes=messages)
-
-        if count:
-            post_message_to_mattermost(text, config, team)
+    '''Every team has its own configuration'''
+    for team in get_teams(ui):
+        publish(node, repo, ui, team)
 
 
-def get_changesets(repo, node):
+def publish(node, repo, ui, team):
+    config = get_config(ui, team)
+    changesets = get_changesets(repo, node, config.branches)
+    count = len(changesets)
+    if not count:
+        return
+    # filter some characters that cause http 400 response
+    messages = re.sub(
+        '[;]', '',
+        render_changesets(ui, repo, changesets, config)
+    )
+
+    ensure_plural = "s" if count > 1 else ""
+    ensure_repo_name = " to \"{0}\"".format(os.path.basename(repo.root))
+
+    text = "{user} pushes {count} changeset{ensure_plural}{ensure_repo_name}:\n{changes}".format(
+        user=ui.username() or config.username,
+        count=count,
+        ensure_plural=ensure_plural,
+        ensure_repo_name=ensure_repo_name,
+        changes=messages)
+
+    post_message_to_mattermost(text, config, team)
+
+
+def get_changesets(repo, node, branches):
+    '''
+    Team may share same repository with other team,
+    thus filter team specific branches,
+    otherwise don't filter
+    '''
     node_rev = repo[node].rev()
     tip_rev = repo['tip'].rev()
 
-    def belongs_myproject(branch_name):
+    def belongs_team(branch):
+        # pass any revision branch when team branches not configured
+        if not branches:
+            return True
+        # verify revision branch conforms to team configured branches
         return any(
-            myproject_branch_name in branch_name
-            for myproject_branch_name in '''
-                otherproject1
-                otherproject2
-                myproject
-            '''.split()
+            team_branch in branch
+            for team_branch in branches
         )
 
-    changesets = dict(
-        default = [
-            rev
-            for rev in range(tip_rev, node_rev - 1, -1)
-            if not belongs_myproject(repo[rev].branch())
-        ],
-        myproject = [
-            rev
-            for rev in range(tip_rev, node_rev - 1, -1)
-            if belongs_myproject(repo[rev].branch())
-        ]
-    )
-    return changesets
+    return [
+        rev
+        for rev in range(tip_rev, node_rev - 1, -1)
+        if belongs_team(repo[rev].branch())
+    ]
 
 
 def render_changesets(ui, repo, changesets, config):
-    url = "http://192.168.200.69:8008/{}/rev/".format(os.path.basename(repo.root))
-    if url:
+    url = "{}/{}/rev/".format(config.commit_url, os.path.basename(repo.root))
+    if config.commit_url:
         node_template = "<{url}{{node|short}}|{{node|short}}>".format(url=url)
     else:
         node_template = "{node|short}"
